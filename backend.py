fe71da1763c7cf47754e20167da9bdcd3602a055
ndaelman <ndaelman@physik.hu-berlin.de>
2024-05-06 20:24:53 +0200

    - Handle large NOMAD query responses - Switch to `chat` endpoint in LLama

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import sys
+import time
 
 # Query NOMAD
 upload_id = "-jbjmBQ4SNSMi9CkaXhhug"
@@ -38,7 +39,9 @@ nomad_params = {
 
 def llama_complete(llama_prompt: dict[str, str]) -> dict[str, str]:
     llama_prompt['model'] = "llama3:70b"
+    llama_prompt['seed'] = 42
     llama_prompt['temperature'] = 0.05
+    llama_prompt['stream'] = False
     return llama_prompt
 
 def llama_inject_context(llama_prompt: dict[str, str], llama_responses: list[dict[str, str]]) -> dict[str, str]:
@@ -47,45 +50,62 @@ def llama_inject_context(llama_prompt: dict[str, str], llama_responses: list[dic
     return llama_prompt
 
 def llama_response_to_list(llama_response: dict[str, str]) -> list[dict[str, str]]:
-    extracted: list[dict[str, str]] = []
-    for x in llama_response.content.decode('utf-8').strip().split('\n'):
-        x = json.loads(x)
-        if 'response' in x:
-            extracted.append(x)
-        elif 'error' in x:
-            print("Error in llama response:", x)
-            sys.exit(1)
-    return extracted
+    return json.loads(llama_response.content.decode('ascii').strip())['message']['content']
 
 # Check if the NOMAD query was successful
 nomad_results, nomad_metadata = [], []
-
+trial_counter, max_trials = 0, 10
 while True:
     nomad_response = requests.post(nomad_url, json=nomad_params)
-
+    trial_counter += 1
     if nomad_response.status_code == 200:
         nomad_data = nomad_response.json()
         if (next_page := nomad_data['pagination'].get('next_page_after_value', '')):
             nomad_params["pagination"]["page_after_value"] = next_page
             nomad_results.extend([entry["archive"]['results'] for entry in nomad_data['data']])
             nomad_metadata.extend([entry["archive"]['metadata'] for entry in nomad_data['data']])
+            trial_counter = 0 # reset the trial counter
         else:
             break
+    elif trial_counter >= max_trials:
+        print(f"Failed to query NOMAD after {max_trials} trials.")
+        sys.exit(1)
+    elif nomad_response.status_code in (502, 503):
+        print("Retrying query to NOMAD...")
+        time.sleep(10)
     else:
         print("Failed to query NOMAD:", nomad_response.text)
         sys.exit(1)  # ! add error handling
 print(f"Found {len(nomad_results)} results in NOMAD for upload_id {upload_id}. Analyzing...")
 
 # Push a llama query
-llama_url = "http://172.28.105.30/backend/api/generate"
+llama_url = "http://172.28.105.30/backend/api/chat"
 llama_init_params = {
-    "prompt": """
-        Write me a Methods section for a paper draft. Keep it short and descriptive, 250 words max.
-        Focus on providing an overview of the data (mentioned below) and avoid giving reasons. No citations.
-        Start immediately with the suggestion. Use a formal tone and the past tense.
-        Here is a json format overview of the settings:
-    """.strip().replace("\n", "") + '\n' + str(nomad_results),
+    "messages": [
+        {
+            "role": "system",
+            "content": """
+                You are a data analyst, summarizing data from a database.
+                You give me short (max 250 words) responses to my prompts.
+                You stay on-topic and provide relevant information.
+                No disclaimer or suggestions for improvements.
+            """.strip().replace("\n", ""),
+        },
+        {
+            "role": "user",
+            "content": """
+                Write me a Methods section for a paper draft.
+                Here is a json response from the database API:
+            """.strip().replace("\n", ""),
+        },
+    ]  
 }
+for nomad_result in nomad_results:
+    llama_init_params["messages"].append({
+        "role": "user",
+        "content": str(nomad_result),
+    })
+
 llama_citation = {
     "prompt": """
         Amend the previous response by adding citations. Say that a 'similar setup as in [1]' was used.
@@ -104,7 +124,7 @@ else:
     print("Failed to push llama query:", llama_init_response.text)
 
 # ! add citations
-if any([len(x['datasets']) for x in nomad_metadata]):
+if any([len(x['datasets']) for x in nomad_metadata]) and False:  # deactivated
     llama_citation_response = requests.post(llama_url, json=llama_complete(llama_inject_context(llama_citation, llama_init_data)))
     if llama_citation_response.status_code == 200:
         print(''.join([x["response"] for x in llama_response_to_list(llama_citation_response)]))
