9c725852d045d6559292dcc0a987f40344be9989
Nathan Daelman <ndaelman.physik.hu-berlin.de>
2024-05-09 18:01:34 +0200

    - Feed data in smaller portions - Enhance prompt

--- query_llama.py
+++ query_llama.py
@@ -2,19 +2,18 @@ import json
 from typing import Optional
 
 def llama_complete_prompt(
-    query_file: str,
+    llama_prompt: dict[str, str],
     temperature: float = 0,
     seed: Optional[int] = 42,
 ) -> dict[str, str]:
     """Complete a prompt for the llama model."""
-    with open(query_file, "r") as f:
-        llama_prompt = json.load(f)
-        llama_prompt['model'] = "llama3:70b"
-        llama_prompt['options'] = {"temperature": temperature}
-        if seed is not None:
-            llama_prompt['options']['seed'] = seed
-        llama_prompt['stream'] = False
+    llama_prompt['model'] = "llama3:70b"
+    llama_prompt['options'] = {"temperature": temperature}
+    if seed is not None:
+        llama_prompt['options']['seed'] = seed
+    llama_prompt['stream'] = False
     return llama_prompt
+
 
 def llama_inject_context(
     llama_prompt: dict[str, str],
@@ -25,6 +24,7 @@ def llama_inject_context(
     llama_prompt['context'] = response_end['context'] if response_end['done'] else []
     return llama_prompt
 
+
 def llama_response_to_list(llama_response: dict[str, str]) -> list[dict[str, str]]:
     """Function for converting a llama response"""
-    return json.loads(llama_response.content.decode('utf-8').strip())['message']['content']
+    return json.loads(llama_response.content.decode('utf-8').strip())
